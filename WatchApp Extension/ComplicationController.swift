08653a49b7394105aadf560762a272b25d1b9629
Wilsonator5000 <wilsonator5000@gmail.com>
2017-06-14 15:31:24 -0400

    Version 4.0 - the Watch app works now!

--- WatchApp Extension/ComplicationController.swift
+++ WatchApp Extension/ComplicationController.swift
@@ -7,14 +7,20 @@
 //
 
 import ClockKit
-
+import WatchKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
+    
+    // UserDefaults for app group
+    var appGroup = UserDefaults(suiteName: "group.co.neef.ios.WhiteBoardGroup")
+    
+    // InterfaceController reference
+    //let interfaceController = InterfaceController()
     
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
+        handler(CLKComplicationTimeTravelDirections())
     }
     
     func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
@@ -31,10 +37,43 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     // MARK: - Timeline Population
     
+    // Keys for accessing the complicationData dictionary.
+    let ComplicationCurrentEntry = "ComplicationCurrentEntry"
+    let ComplicationTextData = "ComplicationTextData"
+    let ComplicationShortTextData = "ComplicationShortTextData"
+    
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        
+        var entry : CLKComplicationTimelineEntry?
+        let now = NSDate()
+        
+        // gets values and makes sure the complication is never nil (provides default value if nil)
+        var wkTodayBoardValue = appGroup?.string(forKey: "wkTodayBoardValue")
+        var wkNoteToSelfBoardValue = appGroup?.string(forKey: "wkNoteToSelfBoardValue")
+        
+        if (wkTodayBoardValue == nil || wkTodayBoardValue == "") && (wkNoteToSelfBoardValue == nil || wkNoteToSelfBoardValue == "") {
+            wkTodayBoardValue = "Write something on"
+            wkNoteToSelfBoardValue = "the iPhone app!" 
+        }
+        
+        // Create the template and timeline entry.
+        if complication.family == .modularLarge { // makes sure complication is modular large
+            let modularLargeTemplate = CLKComplicationTemplateModularLargeStandardBody() // set complication type (modular large)
+            modularLargeTemplate.tintColor = UIColor.cyan // set color of header text
+            modularLargeTemplate.headerTextProvider = CLKSimpleTextProvider(text: "WhiteBoard") // set header text
+            modularLargeTemplate.body1TextProvider = CLKSimpleTextProvider(text: wkTodayBoardValue!) // set first line of body text
+            modularLargeTemplate.body2TextProvider = CLKSimpleTextProvider(text: wkNoteToSelfBoardValue!) // set second line of body text
+            
+            // Create the entry.
+            entry = CLKComplicationTimelineEntry(date: now as Date, complicationTemplate: modularLargeTemplate)
+        } else {
+            // ...configure entries for other complication families.
+        }
+        
+        // Pass the timeline entry back to ClockKit.
+        handler(entry)
     }
+    
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries prior to the given date
@@ -50,7 +89,34 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+//        handler(nil)
+        
+        var template: CLKComplicationTemplate?
+        switch complication.family {
+        case .modularSmall:
+            template = nil
+        case .modularLarge:
+            let modularLargeTemplate = CLKComplicationTemplateModularLargeStandardBody()
+            modularLargeTemplate.headerTextProvider =
+                CLKSimpleTextProvider(text: "WhiteBoard")
+            modularLargeTemplate.body1TextProvider =
+                CLKSimpleTextProvider(text: (appGroup?.string(forKey: "wkTodayBoardValue") ?? "Write something on"))
+            modularLargeTemplate.body2TextProvider =
+                CLKSimpleTextProvider(text: (appGroup?.string(forKey: "wkNoteToSelfBoardValue") ?? "the iPhone app!"))
+            modularLargeTemplate.tintColor = UIColor.cyan
+            template = modularLargeTemplate
+        case .utilitarianSmall:
+            template = nil
+        case .utilitarianLarge:
+            template = nil
+        case .circularSmall:
+            template = nil
+        case .utilitarianSmallFlat:
+            template = nil
+        case .extraLarge:
+            template = nil
+        }
+        handler(template)
     }
     
 }
--- WatchApp Extension/InterfaceController.swift
+++ WatchApp Extension/InterfaceController.swift
@@ -8,11 +8,17 @@
 
 import WatchKit
 import Foundation
+import WatchConnectivity
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
 
     @IBOutlet var todayBoard: WKInterfaceLabel!
     @IBOutlet var noteToSelfBoard: WKInterfaceLabel!
+    
+    var watchSession : WCSession?
+    
+    var todayBoardText = "nil"
+    var noteToSelfBoardText = "nil"
     
     // UserDefaults for app group
     var appGroup = UserDefaults(suiteName: "group.co.neef.ios.WhiteBoardGroup")
@@ -23,26 +29,56 @@ class InterfaceController: WKInterfaceController {
         // Configure interface objects here.
     }
     
+    @IBAction func sendMessage(sender: AnyObject) {
+        watchSession?.sendMessage(
+            ["message":"increment"],
+            replyHandler: { (message: [String : Any]) in
+        },
+            errorHandler: { (err: Error) in
+        })
+    }
+    
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
+        if (WCSession.isSupported()) {
+            watchSession = WCSession.default()
+            // Add self as a delegate of the session so we can handle messages
+            watchSession!.delegate = self
+            watchSession!.activate()
+        }
+        
         // UserDefaults!! Much simpler this way
-        var todayBoardText = appGroup?.string(forKey: "todayBoardValue")
-        var noteToSelfBoardText = appGroup?.string(forKey: "noteToSelfBoardValue")
-        
-        print("Today text is: \(todayBoardText)")
-        print("NTS text is: \(noteToSelfBoardText)")
-        
-        // Set labels
-        self.todayBoard.setText(todayBoardText)
-        self.noteToSelfBoard.setText(noteToSelfBoardText)
-        
+        todayBoard.setText(appGroup?.string(forKey: "wkTodayBoardValue"))
+        noteToSelfBoard.setText(appGroup?.string(forKey: "wkNoteToSelfBoardValue"))
     }
     
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        NSLog("Watch session activated")
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        // Sets labels
+        todayBoard.setText((message["today"] as! String))
+        noteToSelfBoard.setText((message["nts"] as! String))
+        
+        // UserDefaults!! Much simpler this way
+        appGroup?.set(message["today"] as! String, forKey: "wkTodayBoardValue")
+        appGroup?.set(message["nts"] as! String, forKey: "wkNoteToSelfBoardValue")
+        appGroup?.synchronize()
+        
+        let server = CLKComplicationServer.sharedInstance()
+        for complication: CLKComplication in server.activeComplications! {
+            server.reloadTimeline(for: complication)
+        }
+        
+        print("Successfully recieved data from iPhone. [InterfaceController.swift:82]")
+        replyHandler([:])
+    }
 }
